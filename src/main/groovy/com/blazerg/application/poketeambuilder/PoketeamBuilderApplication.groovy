95463e4322cad9aa9b44df08cbdf170dae0b0046
Blazerg <tnii.gerson@gmail.com>
2017-11-30 22:01:40 +0100

    initial commit

---
+++ src/main/groovy/com/blazerg/application/poketeambuilder/PoketeamBuilderApplication.groovy
@@ -0,0 +1,12 @@
+package com.blazerg.application.poketeambuilder
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PoketeamBuilderApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run PoketeamBuilderApplication, args
+	}
+}
