793180a3e29d3a094fda7d0862886a59b246cc32
Blazerg <tnii.gerson@gmail.com>
2017-11-30 22:07:42 +0100

    first controller to test heroku

--- src/main/groovy/com/blazerg/application/poketeambuilder/PoketeamBuilderApplication.groovy
+++ src/main/groovy/com/blazerg/application/poketeambuilder/PoketeamBuilderApplication.groovy
@@ -1,12 +1,30 @@
 package com.blazerg.application.poketeambuilder
 
+import io.swagger.annotations.Api
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
+@Api
+@Controller
+@ComponentScan(basePackages = 'com.blazerg.application.poketeambuilder')
+@EnableJpaRepositories(basePackages = 'com.blazerg.application.poketeambuilder.domain.repository')
+@EnableAutoConfiguration
 @SpringBootApplication
 class PoketeamBuilderApplication {
 
-	static void main(String[] args) {
-		SpringApplication.run PoketeamBuilderApplication, args
-	}
+    @RequestMapping(value = "/")
+    @ResponseBody
+    String herokuTest(){
+        "Hello world!"
+    }
+
+    static void main(String[] args) {
+        SpringApplication.run PoketeamBuilderApplication, args
+    }
 }
---
+++ src/main/groovy/com/blazerg/application/poketeambuilder/config/SwaggerConfiguration.groovy
@@ -0,0 +1,58 @@
+package com.blazerg.application.poketeambuilder.config
+
+import io.swagger.annotations.Api
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.ResponseEntity
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+/**
+ * Swagger configuration.
+ */
+@Configuration
+@EnableSwagger2
+class SwaggerConfiguration {
+
+    /**
+     * Create Swagger Api Configuration.
+     *
+     * @return Swagger Docket
+     */
+    @Bean
+    Docket customImplementation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .groupName('poketeam-builder')
+            .apiInfo(apiInfo())
+            .ignoredParameterTypes(MetaClass)
+            .select()
+            .apis(RequestHandlerSelectors.withClassAnnotation(Api))
+            .paths(PathSelectors.any())
+            .build()
+            .pathMapping('/')
+            .genericModelSubstitutes(ResponseEntity)
+            .useDefaultResponseMessages(false)
+    }
+
+    /**
+     * Generate Api Info.
+     *
+     * @return Swagger API Info
+     */
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title('Poke team builder for the poke virtual league')
+            .description('Just a project for practicing microservices architecture')
+            .license('Blazerg')
+            .licenseUrl('https://github.com/Blazerg2/')
+            .termsOfServiceUrl('')
+            .version('1.0.0')
+            .build()
+    }
+
+}
