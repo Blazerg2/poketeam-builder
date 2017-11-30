8f4eed5ec2ab588ae7946f4c3577660e9450a44a
Blazerg <tnii.gerson@gmail.com>
2017-11-30 22:22:52 +0100

    import fix

--- src/main/groovy/com/blazerg/application/poketeambuilder/domain/repository/PokemonRepository.groovy
+++ src/main/groovy/com/blazerg/application/poketeambuilder/domain/repository/PokemonRepository.groovy
@@ -1,6 +1,6 @@
 package com.blazerg.application.poketeambuilder.domain.repository
 
-import com.blazerg.application.pokevleague.domain.entity.Pokemon
+import com.blazerg.application.poketeambuilder.domain.entity.Pokemon
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
